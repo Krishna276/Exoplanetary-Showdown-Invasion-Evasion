948ea6e239485009fa57ce2c67eef05e6cb574de
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-03-29 20:24:50 +1300

    Delete constants.py
    constants.py is no longer needed, its job is done better now by __init__.py for the constants package.

---
+++ src/constants/__init__.py
@@ -0,0 +1,24 @@
+"""Loads all the constants for the game."""
+
+from json import load as _load
+from pathlib import Path as _Path
+
+__all__ = ['ALIENS', 'GAME_SETTINGS', 'TILES', 'TURRETS']
+
+ALIENS: dict = {}
+GAME_SETTINGS: dict = {}
+TILES: dict = {}
+TURRETS: dict = {}
+
+_path = str(_Path(__file__).parent.absolute())
+
+with (
+    open(_path + '/aliens.json', 'r') as aliens,
+    open(_path + '/game_settings.json', 'r') as game_settings,
+    open(_path + '/tiles.json', 'r') as tiles,
+    open(_path + '/turrets.json', 'r') as turrets
+):
+    ALIENS = _load(aliens)
+    GAME_SETTINGS = _load(game_settings)
+    TILES = _load(tiles)
+    TURRETS = _load(turrets)
