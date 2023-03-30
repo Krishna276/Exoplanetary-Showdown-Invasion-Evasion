fe6aead5a2b6aaafd5cd35244c32be57c6fd5833
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-03-31 08:47:35 +1300

    Added a constant REPO_PATH
    This is the path to the repo's directory, accessed to any file by absolute path can now be done by concatenating the rest of the path to this.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,24 +1,23 @@
 """Loads all the constants for the game."""
+from json import load
+from pathlib import Path 
 
-from json import load as _load
-from pathlib import Path as _Path
-
-__all__ = ['ALIENS', 'GAME_SETTINGS', 'TILES', 'TURRETS']
+__all__ = ['ALIENS', 'GAME_SETTINGS', 'TILES', 'TURRETS', 'REPO_PATH']
 
 ALIENS: dict = {}
 GAME_SETTINGS: dict = {}
 TILES: dict = {}
 TURRETS: dict = {}
 
-_path = str(_Path(__file__).parent.absolute())
+REPO_PATH = str(Path(__file__).parent.parent.parent.absolute())
 
 with (
-    open(_path + '/json/aliens.json', 'r') as aliens,
-    open(_path + '/json/game_settings.json', 'r') as game_settings,
-    open(_path + '/json/tiles.json', 'r') as tiles,
-    open(_path + '/json/turrets.json', 'r') as turrets
+    open(REPO_PATH + '/src/constants/json/aliens.json', 'r') as aliens,
+    open(REPO_PATH + '/src/constants/json/game_settings.json', 'r') as game_settings,
+    open(REPO_PATH + '/src/constants/json/tiles.json', 'r') as tiles,
+    open(REPO_PATH + '/src/constants/json/turrets.json', 'r') as turrets
 ):
-    ALIENS = _load(aliens)
-    GAME_SETTINGS = _load(game_settings)
-    TILES = _load(tiles)
-    TURRETS = _load(turrets)
+    ALIENS = load(aliens)
+    GAME_SETTINGS = load(game_settings)
+    TILES = load(tiles)
+    TURRETS = load(turrets)
