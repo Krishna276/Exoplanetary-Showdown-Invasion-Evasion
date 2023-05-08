eeb7ffdb227efa904a74eda76536d53930d88869
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-05-08 20:57:40 +1200

    Fixed all our imports
    Turns out it wasn't in PYTHONPATH this whole time. Now the code automatically adds it.

--- src/functions/generation.py
+++ src/functions/generation.py
@@ -1,11 +1,11 @@
 """This file generates things like the map."""
 
-from ..classes.grid import Grid
-from ..classes.tile import Tile
-from ..classes.vector import Vector
+from src.classes.grid import Grid
+from src.classes.tile import Tile
+from src.classes.vector import Vector
 
 def generateMap(width: int, height: int) -> Grid[Tile]:
-    """Generated the map.
+    """Generates the map.
 
     Args:
         width (int): Width of the map to generate.
@@ -14,9 +14,12 @@ def generateMap(width: int, height: int) -> Grid[Tile]:
     Returns:
         Grid[Tile]: A Grid object representing the generated map.
     """
-    map_ = Grid(Vector(width, height))
-    # The rules fofr map generation have not been determined yet.
+    map_: Grid[Tile] = Grid[Tile](Vector(width, height))
+    # The rules for map generation have not been determined yet.
     # Fill everything with empty space or vacuum tiles.
+    for i in range(height):
+        for j in range(width):
+            map_[i, j] = Tile()
     # Place the tiles with effects (like slowness). Order and probabilities of these is TBD.
     # Place portal tile(s) (these will likely be determined by a game setting).
     # Place the earth tile (again, probably a game setting).
