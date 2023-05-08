7c821d4109ce1375c3d433a04029e1adf2006fe0
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-05-08 20:58:57 +1200

    The ROOT update
    The constants package now allows you to access the root directory of this application with a single constant. So when a file is needed, all we need to do is concatenate the path from the root on.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,21 +1,22 @@
 """Loads all the constants for the game."""
-from json import load
-from pathlib import Path 
 
-__all__ = ['ALIENS', 'GAME_SETTINGS', 'TILES', 'TURRETS', 'REPO_PATH']
+__all__ = ['ALIENS', 'GAME_SETTINGS', 'TILES', 'TURRETS', 'ROOT']
+
+from json import load
+from pathlib import Path
 
 ALIENS: dict = {}
 GAME_SETTINGS: dict = {}
 TILES: dict = {}
 TURRETS: dict = {}
 
-REPO_PATH = str(Path(__file__).parent.parent.parent.absolute())
+ROOT = str(Path(__file__).parent.parent.parent.absolute())
 
 with (
-    open(REPO_PATH + '/src/constants/json/aliens.json', 'r') as aliens,
-    open(REPO_PATH + '/src/constants/json/game_settings.json', 'r') as game_settings,
-    open(REPO_PATH + '/src/constants/json/tiles.json', 'r') as tiles,
-    open(REPO_PATH + '/src/constants/json/turrets.json', 'r') as turrets
+    open(ROOT + '/src/constants/json/aliens.json', 'r') as aliens,
+    open(ROOT + '/src/constants/json/game_settings.json', 'r') as game_settings,
+    open(ROOT + '/src/constants/json/tiles.json', 'r') as tiles,
+    open(ROOT + '/src/constants/json/turrets.json', 'r') as turrets
 ):
     ALIENS = load(aliens)
     GAME_SETTINGS = load(game_settings)
