62b8946540f3bc38d1971d2eaa682ab81bd2e94e
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-05-08 20:59:38 +1200

    Create main.py
    The main program has been created finally. It doesn't do much right now though.

---
+++ src/main.py
@@ -0,0 +1,22 @@
+"""Exoplanetary Showdown: Invasion Evasion
+
+A game made by CoDerection.
+
+This code is open-soucre and availabie on the following GitHub repository:
+
+[Krishna276/Exoplanetary-Showdown-Invasion-Evasion]\
+(https://github.com/Krishna276/Exoplanetary-Showdown-Invasion-Evasion)
+"""
+
+from sys import path
+
+from constants import ROOT
+
+path.append(ROOT)
+
+def main() -> None:
+    """Run the program."""
+    pass
+
+if __name__ == '__main__':
+    main()
