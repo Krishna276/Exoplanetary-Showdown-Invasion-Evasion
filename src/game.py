638416e86184b18ef0c1b4f98ef1e9e072bad249
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-05-30 09:21:02 +1200

    Create game.py
    The Game class (and its module) now exist.

---
+++ src/game.py
@@ -0,0 +1,17 @@
+"""Module that runs the game, including all top-level gameplay functions and managing things like sprites."""
+
+class Game:
+    """A class that runs the game.
+    Objects of this class can be saved to allow for game saves
+    (e.g., if either player needs to go but doesn't want to forfeit).
+    """
+    def __init__(self) -> None:
+        """A class that runs the game.
+        Objects of this class can be saved to allow for game saves
+        (e.g., if either player needs to go but doesn't want to forfeit).
+        """
+        pass
+
+    def play(self) -> None:
+        """Play the game."""
+        pass
