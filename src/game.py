452151af038d909597200a7eef2fd976e48644ab
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-07-17 21:35:34 +1200

    Update game.py
    At last PyGame is here. There is a lot to go but at least this is a beginning. Currently all this does is creates a screen and updates frames (of a black screen for now).

--- src/game.py
+++ src/game.py
@@ -1,17 +1,45 @@
 """Module that runs the game, including all top-level gameplay functions and managing things like sprites."""
 
-class Game:
-    """A class that runs the game.
-    Objects of this class can be saved to allow for game saves
-    (e.g., if either player needs to go but doesn't want to forfeit).
-    """
-    def __init__(self) -> None:
-        """A class that runs the game.
-        Objects of this class can be saved to allow for game saves
-        (e.g., if either player needs to go but doesn't want to forfeit).
-        """
-        pass
+import pygame
+import pygame_gui
 
+from constants import GAME_SETTINGS, ROOT
+
+class Game:
+    """A class that runs the game."""
+    def __init__(self) -> None:
+        pygame.init()
+        scale: float = 0
+        self.window_width: int = int(GAME_SETTINGS['window']['width'] * scale)
+        self.window_height: int = int(GAME_SETTINGS['window']['height'] * scale)
+        self.screen: pygame.Surface = pygame.display.set_mode(
+            (self.window_width, self.window_height)
+        )
+        pygame.display.set_caption('Exoplanetary Showdown: Invasion Evasion')
+        # pygame.display.set_icon(pygame.image.load(ROOT + GAME_SETTINGS['window']['icon']).convert())
+        self.aliens: pygame.sprite.Group = pygame.sprite.Group()
+        self.turrets: pygame.sprite.Group = pygame.sprite.Group()
+        self.all_sprites: pygame.sprite.Group = pygame.sprite.Group()
+        self.ui_manager: pygame_gui.UIManager = pygame_gui.UIManager(
+            (self.window_width, self.window_height)
+        )
+        self.clock: pygame.time.Clock = pygame.time.Clock()
+        self.running: bool = True
+    
+    def _kill(self) -> None:
+        self.running = False
+    
     def play(self) -> None:
-        """Play the game."""
-        pass
+        while self.running:
+            self._loop()
+
+    def _loop(self) -> None:
+        dt: int = self.clock.tick()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._kill()
+            self.ui_manager.process_events(event)
+        self.ui_manager.update(pygame.time.get_ticks())
+        self.ui_manager.draw_ui(self.screen)
+        pygame.display.flip()
+
