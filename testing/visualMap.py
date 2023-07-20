1fcf093c49eebbd28b66a27be12f97c554dbf255
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-07-20 16:02:21 +1200

    Update visualMap.py
    Small changes to the map visualizer, including using the new constants.

--- testing/visualMap.py
+++ testing/visualMap.py
@@ -5,10 +5,8 @@ path.append(input('Enter repo root path: '))
 from PySimpleGUI import Button, Input, Frame, Text, Window, WIN_CLOSED
 
 from src.classes.tile import TileType
-from src.constants import GAME_SETTINGS, TILES
+from src.constants import BF_GRID_HEIGHT, BF_GRID_WIDTH, TILES
 from src.functions.generation import generateMap
-
-WIDTH, HEIGHT = GAME_SETTINGS['battlefield']['width'], GAME_SETTINGS['battlefield']['height']
 
 COLOURS = [
     '#555555',
@@ -25,7 +23,7 @@ COLOURS = [
 
 # PySimpleGUI.theme(theme: str) to change theme
 
-map_, earth, portal = generateMap(WIDTH, HEIGHT, '0')
+map_, earth, portal = generateMap(BF_GRID_WIDTH, BF_GRID_HEIGHT, '')
 
 def getColour(x: int, y: int) -> str:
     return COLOURS[map_[x, y].tileType.value]
@@ -36,11 +34,13 @@ def compliment(hex_color: str) -> str:
 layout = [
     [Text('Visual Map Generator')],
     [Text('Not part of the actual game, but this is only to visualise the map generation and pathfinding algorithms.')],
-    [Frame('Map', [
-        [Text('    ', background_color=getColour(j, i), key=f'-{i},{j}-') for j in range(WIDTH)] for i in range(HEIGHT)
-    ])],
-    [Text('Seed: '), Input('0', key='-seed-'), Button('Generate Map'), Button('Pathfind')],
-    [Text('Key:')] + [Text(TILES[TileType(i).name]['name'], background_color=COLOURS[i], text_color=compliment(COLOURS[i])) for i in range(10)]
+    [Frame('Map', [[
+        Text('    ', background_color=getColour(j, i), key=f'-{i},{j}-') for j in range(BF_GRID_WIDTH)
+    ] for i in range(BF_GRID_HEIGHT)])],
+    [Text('Seed: '), Input('', key='-seed-'), Button('Generate Map'), Button('Pathfind')],
+    [Text('Key:')] + [Text(
+        TILES[TileType(i).name]['name'], background_color=COLOURS[i], text_color=compliment(COLOURS[i])
+    ) for i in range(10)]
 ]
 
 # Create a PySimpleGUI window
@@ -52,9 +52,9 @@ while True:
     if event == WIN_CLOSED:
         break
     elif event == 'Generate Map':
-        map_, earth, portal = generateMap(WIDTH, HEIGHT, values['-seed-'])
-        for i in range(HEIGHT):
-            for j in range(WIDTH):
+        map_, earth, portal = generateMap(BF_GRID_WIDTH, BF_GRID_HEIGHT, values['-seed-'])
+        for i in range(BF_GRID_HEIGHT):
+            for j in range(BF_GRID_WIDTH):
                 window[f'-{i},{j}-'].update(background_color=getColour(j, i))
     elif event == 'Pathfind':
         for coordinates in map_.pathfind(portal, earth):
