d8fb546ee7b556eded50673ab72421076c824d3a
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-07-28 08:59:30 +1200

    Update __init__.py
    Made __init__.py imports consistent with the code guide.

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,26 +1,31 @@
 """Module that provides the class that runs the game."""
 
-import pygame
-import pygame_gui
+from pygame import init as init_pygame, Surface, QUIT
+from pygame.display import flip as flip_display, set_caption, set_icon, set_mode
+from pygame.event import get
+from pygame.image import load
+from pygame.sprite import Group
+from pygame.time import Clock, get_ticks
+from pygame_gui import UIManager
 
 from src.constants import ROOT, WINDOW_HEIGHT, WINDOW_WIDTH
 
 class Game:
     """A class that runs the game."""
     def __init__(self) -> None:
-        pygame.init()
-        self.screen: pygame.Surface = pygame.display.set_mode(
+        init_pygame()
+        self.screen: Surface = set_mode(
             (WINDOW_WIDTH, WINDOW_HEIGHT)
         )
-        pygame.display.set_caption('Exoplanetary Showdown: Invasion Evasion')
-        # pygame.display.set_icon(pygame.image.load(ROOT + GAME_SETTINGS['window']['icon']).convert())
-        self.aliens: pygame.sprite.Group = pygame.sprite.Group()
-        self.turrets: pygame.sprite.Group = pygame.sprite.Group()
-        self.all_sprites: pygame.sprite.Group = pygame.sprite.Group()
-        self.ui_manager: pygame_gui.UIManager = pygame_gui.UIManager(
+        set_caption('Exoplanetary Showdown: Invasion Evasion')
+        # set_icon(load(ROOT + GAME_SETTINGS['window']['icon']).convert())
+        self.aliens: Group = Group()
+        self.turrets: Group = Group()
+        self.all_sprites: Group = Group()
+        self.ui_manager: UIManager = UIManager(
             (WINDOW_WIDTH, WINDOW_HEIGHT)
         )
-        self.clock: pygame.time.Clock = pygame.time.Clock()
+        self.clock: Clock = Clock()
         self.running: bool = True
     
     def _kill(self) -> None:
@@ -28,15 +33,15 @@ class Game:
     
     def _update(self) -> None:
         dt: int = self.clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in get():
+            if event.type == QUIT:
                 self._kill()
             self.ui_manager.process_events(event)
         for entity in self.all_sprites:
             self.screen.blit(entity.surf, entity.rect)
-        self.ui_manager.update(pygame.time.get_ticks())
+        self.ui_manager.update(get_ticks())
         self.ui_manager.draw_ui(self.screen)
-        pygame.display.flip()
+        flip_display()
     
     def play(self) -> None:
         while self.running:
