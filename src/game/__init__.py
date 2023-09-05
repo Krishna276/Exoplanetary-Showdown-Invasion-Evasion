6737ad5a626b73005d4919be71f53ea4b57cfc25
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-09-06 10:22:03 +1200

    Update __init__.py
    Started implementing the aliens that will run across the screen.

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,15 +1,24 @@
 """Module that provides the class that runs the game."""
 
 from enum import Enum
+from typing import Self
 
 from pygame import init as init_pygame, Surface, QUIT
 from pygame.display import flip as flip_display, set_caption, set_icon, set_mode
-from pygame.event import get
+from pygame.draw import line as draw_line
+from pygame.event import get as get_events
 from pygame.sprite import Group
 from pygame.time import Clock, get_ticks
 from pygame_gui import UIManager
 
-from src.constants import GAME_SETTINGS, ROOT, WINDOW_HEIGHT, WINDOW_WIDTH
+from src.classes.grid import Grid
+from src.classes.tile import Tile
+from src.classes.vector import Vector
+from src.constants import (
+    BF_GRID_HEIGHT, BF_GRID_WIDTH, BF_TILE_LENGTH, BORDER_HEIGHT, BORDER_WIDTH, GAME_SETTINGS, ROOT, WINDOW_HEIGHT,
+    WINDOW_WIDTH
+)
+from src.functions.generation import generateMap
 from src.functions.load_asset import load_image
 
 class Phase(Enum):
@@ -21,39 +30,51 @@ class Phase(Enum):
 
 class Game:
     """A class that runs the game."""
-    def __init__(self) -> None:
+    def __init__(self: Self) -> None:
         init_pygame()
-        self.screen: Surface = set_mode(
-            (WINDOW_WIDTH, WINDOW_HEIGHT)
-        )
+        self.screen: Surface = set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         set_caption('Exoplanetary Showdown: Invasion Evasion')
         set_icon(load_image(ROOT + GAME_SETTINGS['window']['icon']).convert())
         self.aliens: Group = Group()
         self.turrets: Group = Group()
         self.all_sprites: Group = Group()
-        self.ui_manager: UIManager = UIManager(
-            (WINDOW_WIDTH, WINDOW_HEIGHT)
-        )
+        self.tile_sprites: Group = Group()
+        self.ui_manager: UIManager = UIManager((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock: Clock = Clock()
         self.running: bool = True
         self.phase: Phase = Phase.INIT
+        self.map: Grid[Tile]
+        self.mothership: Vector
+        self.earth: Vector
+        self.map, self.earth, self.mothership = generateMap(BF_GRID_WIDTH, BF_GRID_HEIGHT, '')
     
-    def _kill(self) -> None:
+    def _kill(self: Self) -> None:
         self.running = False
     
-    def _update(self) -> None:
+    def _draw_grid(self: Self) -> None:
+        x = BORDER_WIDTH
+        for _ in range(BF_GRID_WIDTH + 1):
+            draw_line(self.screen, '#FFFFFF', (x, BORDER_HEIGHT), (x, WINDOW_HEIGHT - BORDER_HEIGHT))
+            x += BF_TILE_LENGTH
+        y = BORDER_HEIGHT
+        for _ in range(BF_GRID_HEIGHT + 1):
+            draw_line(self.screen, '#FFFFFF', (BORDER_WIDTH, y), (WINDOW_WIDTH - BORDER_WIDTH, y))
+            y += BF_TILE_LENGTH
+    
+    def _update(self: Self) -> None:
         dt: int = self.clock.tick(60)
-        for event in get():
+        for event in get_events():
             if event.type == QUIT:
                 self._kill()
             self.ui_manager.process_events(event)
+        self._draw_grid()
         for entity in self.all_sprites:
             self.screen.blit(entity.surf, entity.rect)
         self.ui_manager.update(get_ticks())
         self.ui_manager.draw_ui(self.screen)
         flip_display()
     
-    def play(self) -> None:
+    def play(self: Self) -> None:
         while self.running:
             self._update()
 
