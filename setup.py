4a6802b4adad65e1bc4823d279861a5e59ad4f16
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-09-08 09:32:30 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 """Setup script for Exoplanetary Showdown: Invasion Evasion.
 
 Please ensure that this file is run from the directory you want to install the game into.
+
+This file is not a very technical one. Use main.py to chose a seed.
 """
 
-from os import getcwd, system
-from runpy import run_path
-from sys import path
+from os import system
 
 system('pip install pygame')
 system('pip install pygame-gui')
