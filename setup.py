23ecfe251e7d2ef6993e969cd824010978919324
Krishna Ranchhod <67448147+Krishna276@users.noreply.github.com>
2023-09-10 11:23:53 +1200

    Update setup.py
    Modified the setup script to automatically install pip too.

--- setup.py
+++ setup.py
@@ -5,8 +5,29 @@ Please ensure that this file is run from the directory you want to install the g
 This file is not a very technical one. Use main.py to chose a seed.
 """
 
-from os import system
+from ensurepip import bootstrap
+from os import system as run_command
+from pathlib import Path
+from platform import system as operating_system
 
-system('pip install pygame')
-system('pip install pygame-gui')
-system('python src/main.py')
+DIRECTORY: str = Path(__file__).parent.absolute()
+
+bootstrap()
+OS: str = operating_system()
+match OS:
+    case 'Windows':
+        print('Operating system detected: Windows.')
+        run_command('py -m pip install pygame')
+        run_command('py -m pip install pygame-gui')
+        run_command(f'py {DIRECTORY}\\src\\main.py')
+    case 'Linux':
+        print('Operating system detected: Linux.')
+        run_command('python3 -m pip install pygame')
+        run_command('python3 -m pip install pygame-gui')
+        run_command(f'python3 {DIRECTORY}/src/main.py')
+    case _:
+        print(
+            'Sorry, your OS is not supported yet by the setup script.\n'
+            'Things you can try:\n'
+            '    Install dependencies pip, pygame and pygame-gui yourself, then run src/main.py'
+        )
